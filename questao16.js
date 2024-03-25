8a131365a202967ed8873e8514a67542078976ad
thaua-arruda <thauafarruda@gmail.com>
2024-03-25 17:20:52 -0400

    adicionado questao 16

---
+++ questao16.js
@@ -0,0 +1,8 @@
+/*
+16. Qual é o resultado de (true && true) || (false && false)?
+                               true      ||       false
+                                        true
+
+*/
+(true && true) || (false && false)
+console.log((true && true) || (false && false)) //true
