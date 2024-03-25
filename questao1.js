f5dfc7f0a04c062afd2b136c8d6bb9dccf2f1320
thaua-arruda <thauafarruda@gmail.com>
2024-03-25 14:54:28 -0400

    adicionado questao 1

---
+++ questao1.js
@@ -0,0 +1,10 @@
+/*
+1. O resultado de (10 > 5) && (7 < 9) é verdadeiro ou falso?
+
+                   true && true
+                       true
+*/
+
+(10 > 5) && (7 < 9)
+console.log((10 > 5) && (7 < 9))
+
