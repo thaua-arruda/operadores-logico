eea6630484b415e47d1c5958837f89db760f6619
thaua-arruda <thauafarruda@gmail.com>
2024-03-25 17:19:24 -0400

    adicionado questao 5

---
+++ questao5.js
@@ -0,0 +1,7 @@
+/*
+5. Qual é o resultado de (8 !== 8) || (3 <= 3)?
+                           false   ||   true
+                                  true 
+*/
+(8 !== 8) || (3 <= 3)
+console.log((8 !== 8) || (3 <= 3)) //true
