1d3221d6ec60bb0a8a73f03ba9d25ee9c5a02254
thaua-arruda <thauafarruda@gmail.com>
2024-03-25 17:21:46 -0400

    adicionado questao 20

---
+++ questao20.js
@@ -0,0 +1,7 @@
+/*
+20. Qual é o resultado de (5 >= 5) && (10 === 10)?
+                            true   &&    true
+                                  true
+*/
+(5 >= 5) && (10 === 10)
+console.log((5 >= 5) && (10 === 10)) // true
