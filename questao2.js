d2259a87c4d9e33515897d0949bce6907bf10959
thaua-arruda <thauafarruda@gmail.com>
2024-03-25 15:26:50 -0400

    adicionado questao 2 correta

--- questao2.js
+++ questao2.js
@@ -1,5 +1,8 @@
 /*
 2. O que retorna a expressão (true || false) && (false || true)?
+                                  true       &&         true
+                                            true
 */
+
 (true || false) && (false || true)
 console.log((true || false) && (false || true))
