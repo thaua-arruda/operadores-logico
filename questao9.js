372ea30c07d400a886224028f426ccc8264890eb
thaua-arruda <thauafarruda@gmail.com>
2024-03-25 17:20:05 -0400

    adicionado questao 9

---
+++ questao9.js
@@ -0,0 +1,7 @@
+/*
+9. O que retorna a expressão false && (true || false)?
+                            false  &&  true
+                                  false
+*/
+false && (true || false)
+console.log(false && (true || false)) //false
