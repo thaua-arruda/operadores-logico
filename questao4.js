87fd8825369074fdb7ef7d68e5dbb0dabb887d51
thaua-arruda <thauafarruda@gmail.com>
2024-03-25 15:36:26 -0400

    adicionado questao 4

---
+++ questao4.js
@@ -0,0 +1,9 @@
+/*
+4. Explique o que significa a expressão !(true && false).
+*/
+
+/* A exclamação significa "não", então resolvendo primeiro os parenteses (true && false) 
+ Resolvendo os parenteses chegamos ao resultado de "false"
+ descemos o ponto de exclamação ! (não) = !false
+ se a exclamação significa "não" então !false = não falso = verdadeiro
+*/
