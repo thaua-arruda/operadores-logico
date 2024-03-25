546574fdf2cd3ecc82352a9bbccd70ab767ae108
thaua-arruda <thauafarruda@gmail.com>
2024-03-25 17:20:59 -0400

    adicionado questao 17

---
+++ questao17.js
@@ -0,0 +1,9 @@
+/*
+17. Explique o que significa a expressão !(10 <= 5).
+
+        primeiro fazemos o que está entre parenteses (10 <= 5) = false
+        depois é só descer a exclamação que significa "não" = !false
+        !false= não falso = true.
+*/
+!(10 <= 5)
+console.log(!(10 <= 5)) //true
