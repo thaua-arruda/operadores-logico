58491d467f8c7311d48d6cd19cb5ba07b629da90
thaua-arruda <thauafarruda@gmail.com>
2024-03-25 17:20:42 -0400

    adicionado questao 15

---
+++ questao15.js
@@ -0,0 +1,8 @@
+/*
+15. Explique como a ordem de avaliação dos operadores lógicos afeta o 
+resultado de uma expressão em JavaScript.
+    
+ Pois os operadores de maior precedência são avaliados primeiro
+ Exemplo: O operador AND lógico (&&) tem maior precedência que o operador OR lógico (||).
+ Se não seguirmos a ordem de precedência, o resultado final pode dar erro/errado.
+*/
