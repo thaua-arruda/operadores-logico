8d3a3d1ea554a026c0f724a5dfcb766d40a3d959
thaua-arruda <thauafarruda@gmail.com>
2024-03-25 17:20:37 -0400

    adicionado questao 14

---
+++ questao14.js
@@ -0,0 +1,8 @@
+/*
+14. Qual é o resultado de (10 > 5) && !(3 < 1)?
+                            true   &&   !false
+                             true  &&  true
+                                  true
+*/
+(10 > 5) && !(3 < 1)
+console.log((10 > 5) && !(3 < 1)) //true
