63d329bfdf6d3f83f5f0219b65b72b59f5567730
thaua-arruda <thauafarruda@gmail.com>
2024-03-25 17:19:47 -0400

    adicionado questao 7

---
+++ questao7.js
@@ -0,0 +1,7 @@
+/*
+7. Qual é o resultado de true && (false || true)?
+                             true && true
+                                 true
+*/
+true && (false || true)
+console.log(true && (false || true)) //true
