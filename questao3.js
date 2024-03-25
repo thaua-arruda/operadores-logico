23eb4d042dc63ba612e6d719a108fb9daecb07f1
thaua-arruda <thauafarruda@gmail.com>
2024-03-25 15:27:33 -0400

    adicionado questao 3

---
+++ questao3.js
@@ -0,0 +1,10 @@
+/*
+3. Qual é o resultado de !(5 === 3) && (4 >= 4)?
+                            !false  &&  true
+                             true   &&  True
+                                   true
+                                 
+ */
+
+!(5 === 3) && (4 >= 4)
+console.log(!(5 === 3) && (4 >= 4))
