f20f10f7be48d27fea8b7e7937456b7613f562fd
thaua-arruda <thauafarruda@gmail.com>
2024-03-25 17:20:13 -0400

    adicionado questao 10

---
+++ questao10.js
@@ -0,0 +1,8 @@
+/*
+10. Qual é o resultado de !true || false && true?
+                         false  ||  false
+                              false
+*/
+!true || false && true
+console.log(!true || false && true) //false
+
