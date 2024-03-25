32ba205fedffa2856109f399eea744b40c8acd24
thaua-arruda <thauafarruda@gmail.com>
2024-03-25 17:20:31 -0400

    adicionado questao 13

---
+++ questao13.js
@@ -0,0 +1,8 @@
+/*
+13. O que significa a expressão !(5 !== 5)?
+                                  5 "não é igual" a 5 = !false
+                                  !false = true
+                                  
+*/
+!(5 !== 5)
+console.log(!(5 !== 5)) //true
