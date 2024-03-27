1bcc33d03752a92b58bb0b38076b736a7a8f6fa3
thaua-arruda <thauafarruda@gmail.com>
2024-03-27 15:00:25 -0400

    finalizada questao 23

---
+++ questao23.js
@@ -0,0 +1,8 @@
+/*
+23. Qual é o resultado de !(7 > 3) || (6 <= 2)?
+                            !true  ||  false
+                             false || false
+                                 false
+*/                     
+!(7 > 3) || (6 <= 2)
+console.log(!(7 > 3) || (6 <= 2)) //false
