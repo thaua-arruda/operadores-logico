e2442d013c974ad36a8a07a19cd8e34d0ee4018b
thaua-arruda <thauafarruda@gmail.com>
2024-03-27 15:00:45 -0400

    finalizada questao 26

---
+++ questao26.js
@@ -0,0 +1,11 @@
+/*
+26. Explique como os parênteses afetam o resultado de uma expressão com 
+operadores lógicos em JavaScript.
+
+Resposta: Os números que estão dentro dos parênteses tem precedência sobre os numeros que estão fora.
+
+ EXEMPLO:      false && (true || false)?
+               false &&  true
+                   false
+*/
+
