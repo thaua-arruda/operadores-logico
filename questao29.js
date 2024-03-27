c9953af879bf43ef417f461a5d362d961891cdb1
thaua-arruda <thauafarruda@gmail.com>
2024-03-27 15:01:00 -0400

    finalizada questao 29

---
+++ questao29.js
@@ -0,0 +1,7 @@
+/*
+29. O que retorna a expressão (true && false) || (true && true)?
+                                        false ||  true
+                                             true
+*/
+(true && false) || (true && true)
+console.log((true && false) || (true && true)) //true
