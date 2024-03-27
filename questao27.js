47c1598dabfed66e6ba199332087d05ce1b3938a
thaua-arruda <thauafarruda@gmail.com>
2024-03-27 15:00:50 -0400

    finalizada questao 27

---
+++ questao27.js
@@ -0,0 +1,8 @@
+/*
+27. Qual é o resultado de (5 === 5) || !(4 !== 4)?
+                             true   ||   !false
+                              true  ||  true
+                                   true
+
+*/
+console.log((5 === 5) || !(4 !== 4)) //true
