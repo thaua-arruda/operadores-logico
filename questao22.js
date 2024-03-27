f08eca4e0aaa7aff1c88d8901698f873ca6de075
thaua-arruda <thauafarruda@gmail.com>
2024-03-27 15:00:20 -0400

    finalizada questao 22

---
+++ questao22.js
@@ -0,0 +1,8 @@
+/*
+22. O que retorna a expressão false || (true && false)?
+                             false  ||   false
+                                  false
+
+*/
+false || (true && false)
+console.log(false || (true && false)) //false
