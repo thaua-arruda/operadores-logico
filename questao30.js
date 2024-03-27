4bc5d8eebc80a9817c7cb9edd266d7b2b7eb28d3
thaua-arruda <thauafarruda@gmail.com>
2024-03-27 15:01:10 -0400

    finalizada questao 30

---
+++ questao30.js
@@ -0,0 +1,20 @@
+/*
+30. Explique como você pode usar operadores lógicos para verificar se um 
+número é negativo e maior que -10 em JavaScript.
+*/
+
+let numero = -23;
+if(numero < 0 && numero > -10) {
+    console.log(" O número é maior que -10")
+} else {
+    console.log(" O número é menor que -10");
+}
+
+
+
+let algarismo = -3;
+if(algarismo < 0 && algarismo > -10) {
+    console.log(" O número é maior que -10")
+} else {
+    console.log(" O número é menor que -10");
+}
